6cd16df5bb9186ed6f5c091d6518f62803e5af30
Léandre PERRET <leandreperret@gmail.com>
2025-11-28 00:13:06 +0100

    U : Scene perfectly imported, Camera set, End of first day

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { Canvas } from '@react-three/fiber'
 import { PerspectiveCamera } from '@react-three/drei'
-import ComputerDesk from './components/ComputerDesk'
+import Desk from './components/Desk'
 
 export default function App() {
   return (
@@ -9,16 +9,16 @@ export default function App() {
 
         <PerspectiveCamera 
           makeDefault 
-          position={[0, 0.6, 4.5]}
-          rotation={[0,0,0]}
-          fov={50} 
+          position={[0, 1, 1.3]}
+          rotation={[0.05,0,0]}
+          fov={45} 
           near={0.1} 
           far={100}
         />
         
         <ambientLight intensity={0.7} />
         <directionalLight position={[5, 10, 5]} intensity={1.5} />
-        <ComputerDesk />
+        <Desk />
 
       </Canvas>
     </div>
